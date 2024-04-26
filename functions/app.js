381608ef92546779fcb94eeb93ae8909d735f51b
Tokinantenaina <tokib2oba@yahoo.com>
2024-04-27 00:41:42 +0300

    update app

--- functions/app.js
+++ functions/app.js
@@ -7,10 +7,10 @@ const app = express();
 const tvRoutes = require("../tv/tvRoutes");
 const ttRoutes = require("../tt/ttRoutes");
 
-app.get("/", (req, res) => {
+app.get("/.netlify/functions/app", (req, res) => {
   res.send({ message: "Bienvenue sur l'application baiboly-gasy" });
 });
-app.use("/testameta/vaovao", tvRoutes);
-app.use("/testameta/taloha", ttRoutes);
+app.use("/.netlify/functions/app/testameta/vaovao", tvRoutes);
+app.use("/.netlify/functions/app/testameta/taloha", ttRoutes);
 
 module.exports.handler = serverless(app);
