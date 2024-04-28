b58edb34bfcbc7927fb8335e9bb2ba3f6d0e4a53
Tokinantenaina <tokib2oba@yahoo.com>
2024-04-28 11:11:31 +0300

    update export handler

--- tt/readDb.js
+++ tt/readDb.js
@@ -13,68 +13,96 @@ module.exports.readDb = (req, res) => {
     `${ecrivainLowerCase}.json`
   );
 
-  // lecture des db .json
-  fs.access(filepath, fs.constants.F_OK, err => {
-    if (err) {
-      return res.status(404).json({
-        message: "Hamarino Ny mpanoratra ()",
-        err: err,
-        path: `${filepath}`,
-        dirname: `${__dirname}`
+  exports.handler = () => {
+    const directoryPath = "../testameta_taloha";
+
+    try {
+      const files = fs.readdirSync(directoryPath);
+      const data = [];
+
+      files.forEach(file => {
+        const filePath = path.join(directoryPath, file);
+        const fileData = JSON.parse(fs.readFileSync(filePath, "utf8"));
+        data.push(fileData);
       });
-    }
 
-    fs.readFile(filepath, "utf8", (err, data) => {
-      const jsonData = JSON.parse(data);
-      try {
-        if (
-          isNaN(parseInt(toko)) ||
-          isNaN(parseInt(andininy)) ||
-          (farany && isNaN(parseInt(farany)))
-        ) {
-          return res
-            .status(400)
-            .json({ message: "Hamarino ny TOKO sy ny Andininy" });
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
         }
-        let result;
-        result =
-          jsonData[toko] && jsonData[toko][andininy]
-            ? [andininy + "-" + jsonData[toko][andininy]]
-            : ["Tsy hita ity toko sy andininy ity"];
-        farany = farany <= andininy ? 0 : farany;
-        let diff = andininy ? farany - andininy : ".";
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "Erreur lors de la lecture des fichiers JSON."
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      };
+    }
+  };
 
-        if (farany && jsonData[toko] && jsonData[toko][andininy] && diff >= 0) {
-          for (let i = 0; i < diff; i++) {
-            andininy++;
-            if (jsonData[toko][andininy]) {
-              result.push(andininy + "-" + jsonData[toko][andininy]);
-            } else {
-              break;
-            }
+  // lecture des db .json
+
+  fs.readFile(filepath, "utf8", async (err, data) => {
+    console.log(data);
+    if (!data) {
+      return res.status(404).json({ message: "Hamarino Ny mpanoratra ()" });
+    }
+    try {
+      const jsonData = await JSON.parse(data);
+
+      if (
+        isNaN(parseInt(toko)) ||
+        isNaN(parseInt(andininy)) ||
+        (farany && isNaN(parseInt(farany)))
+      ) {
+        return res
+          .status(400)
+          .json({ message: "Hamarino ny TOKO sy ny Andininy" });
+      }
+      let result;
+      result =
+        jsonData[toko] && jsonData[toko][andininy]
+          ? [andininy + "-" + jsonData[toko][andininy]]
+          : ["Tsy hita ity toko sy andininy ity"];
+      farany = farany <= andininy ? 0 : farany;
+      let diff = andininy ? farany - andininy : ".";
+
+      if (farany && jsonData[toko] && jsonData[toko][andininy] && diff >= 0) {
+        for (let i = 0; i < diff; i++) {
+          andininy++;
+          if (jsonData[toko][andininy]) {
+            result.push(andininy + "-" + jsonData[toko][andininy]);
+          } else {
+            break;
           }
         }
-        //SEND
-        andininy = req.params.andininy;
-        const andininyRendu = diff >= 0 ? andininy : andininy + " - ";
-
-        const faranyRendu = parseInt(farany) > andininy ? " - " + farany : "";
-
-        const resultRendu = result.join("");
-
-        if ([result].toString() != "") {
-          res.json({
-            message: `${ecrivain} ${toko} : ${andininyRendu} ${faranyRendu} : ${resultRendu}`
-          });
-        } else {
-          res.status(404).json({
-            message: `${ecrivain} ${toko} : ${andininyRendu} ${faranyRendu}Tsy misy ilay andininy tadiavinao`
-          });
-        }
-      } catch (parseError) {
-        console.error("Erreur de parsing JSON:", parseError);
-        res.status(500).send("Erreur de parsing JSON");
       }
-    });
+      //SEND
+      andininy = req.params.andininy;
+      const andininyRendu = diff >= 0 ? andininy : andininy + " - ";
+
+      const faranyRendu = parseInt(farany) > andininy ? " - " + farany : "";
+
+      const resultRendu = result.join("");
+
+      if ([result].toString() != "") {
+        res.json({
+          message: `${ecrivain} ${toko} : ${andininyRendu} ${faranyRendu} : ${resultRendu}`
+        });
+      } else {
+        res.status(404).json({
+          message: `${ecrivain} ${toko} : ${andininyRendu} ${faranyRendu}Tsy misy ilay andininy tadiavinao`
+        });
+      }
+    } catch (parseError) {
+      console.error("Erreur de parsing JSON:", parseError);
+      res.status(500).send("Erreur de parsing JSON");
+    }
   });
 };
